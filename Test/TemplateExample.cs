f52bf849644e08f8fe3f178641ff387f4010a020
jevlogin <jev.login@mail.ru>
2020-10-25 16:29:50 +0300

    Дополнительное ограничение where T2 : struct

--- Test/TemplateExample.cs
+++ Test/TemplateExample.cs
@@ -7,14 +7,8 @@ namespace JevLogin
     {
         private void Start()
         {
-            //Display(5);
-            //Display(5.0f);
-            //Display(5d);
-            //Display("mama");
-
             var player = FactoryEnemy<Player, float>(777.0f);
             Display($"Speed = {player.Speed}");
-
         }
 
         private void Display<T>(T value)
@@ -23,6 +17,7 @@ namespace JevLogin
         }
 
         private T FactoryEnemy<T, T2>(T2 hp) where T : IInit<T2>, new ()
+            where T2 : struct
         {
             var player = new T();
             player.Init(hp);
