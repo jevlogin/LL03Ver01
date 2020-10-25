8d4564ee73cbb432e8f04b5538967ce0aa0ef500
jevlogin <jev.login@mail.ru>
2020-10-25 17:32:28 +0300

    Индексатор закончили с ним

--- Test/ExampleInterface.cs
+++ Test/ExampleInterface.cs
@@ -8,8 +8,11 @@ namespace JevLogin
     {
         private void Start()
         {
-            User user2 = new User();
+            User user2 = new User(new Vector3[10]);
             user2[0] = new Vector3();
+            var s = user2[MyEnum.None];
+            Debug.Log(s);
+            
 
             return;
             User user = new User
@@ -30,16 +33,53 @@ namespace JevLogin
             Debug.Log(user1);
         }
 
+        public enum MyEnum
+        {
+            None = 0,
+            First = 1,
+            Second = 2,
+            Third = 3
+        }
+
         public class User : ICloneable
         {
             public string Name;
             public Health Health;
             private Vector3[] _points;
 
+            public User()
+            {
+            }
+
+            public User(Vector3[] points)
+            {
+                _points = points;
+            }
+
             public Vector3 this[int i]
             {
                 get { return _points[i]; }
                 set { _points[i] = value; }
+            }
+
+            public string this[MyEnum value]
+            {
+                get
+                {
+                    switch (value)
+                    {
+                        case MyEnum.None:
+                            return MyEnum.None.ToString();
+                        case MyEnum.First:
+                            return MyEnum.First.ToString();
+                        case MyEnum.Second:
+                            return MyEnum.Second.ToString();
+                        case MyEnum.Third:
+                            return MyEnum.Third.ToString();
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(value));
+                    }
+                }
             }
 
             public object Clone()
