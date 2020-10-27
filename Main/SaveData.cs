e96774c05311d2146385d6f3412645f624c66922
jevlogin <jev.login@mail.ru>
2020-10-27 18:57:41 +0300

    Второй вариант – создание обычного необобщенного класса. В этом случае при наследовании у базового класса надо явным образом определить используемый тип

--- Main/SaveData.cs
+++ Main/SaveData.cs
@@ -1,11 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 
 namespace JevLogin
 {
-    public sealed class SaveData<T> : BaseExample<T>
+    public sealed class SaveData : BaseExample<Guid>
     {
-        public SaveData(T id) : base(id)
+        public SaveData(Guid id) : base(id)
         {
             IdPlayer = id;
         }
--- Test/Test.cs
+++ Test/Test.cs
@@ -9,8 +9,8 @@ namespace JevLogin
         private void Start()
         {
             BaseExample<string> data = new BaseExample<string>("name_235");
-            BaseExample<int> data2 = new SaveData<int>(3775);
-            SaveData<Guid> data1 = new SaveData<Guid>(Guid.NewGuid());
+            BaseExample<Guid> data1 = new SaveData(Guid.NewGuid());
+            SaveData data2 = new SaveData(Guid.NewGuid());
         }
     }
 }
