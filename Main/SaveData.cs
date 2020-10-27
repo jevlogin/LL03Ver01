df1d8cf675fd19b8122c12ddc6f0131a77199224
jevlogin <jev.login@mail.ru>
2020-10-27 19:09:10 +0300

    Аргумент типа, предоставляемый в качестве T, должен совпадать с аргументом, предоставляемым в качестве U, или быть производным от него.

--- Main/SaveData.cs
+++ Main/SaveData.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 namespace JevLogin
 {
+    /// <summary>
+    /// Аргумент типа, предоставляемый в качестве T, должен совпадать с аргументом, предоставляемым в качестве U, или быть производным от него.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="R"></typeparam>
+    /// <typeparam name="U"></typeparam>
     public sealed class SaveData<T, R, U> : BaseExample<R>
         where T : U
     {
