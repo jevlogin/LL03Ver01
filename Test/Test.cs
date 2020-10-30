c5498fec41e8fc0ffbcf57e338cf21f4f9fdeb87
jevlogin <jev.login@mail.ru>
2020-10-30 12:53:42 +0300

    обобщенный делегат

--- Test/Test.cs
+++ Test/Test.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 
@@ -7,15 +8,29 @@ namespace JevLogin
 {
     public sealed class Test : MonoBehaviour
     {
+        // Этот обобщенный делегат может вызывать любой метод, который возвращает void и принимает 
+        //  единственный параметр типа
+        private delegate void MyGenericDelegate<T>(T arg);
+        
         private void Start()
         {
-            var objects = new ListInteractableObject();
+            // Зарегистрировать цели
+            MyGenericDelegate<string> strTarget = new MyGenericDelegate<string>(StringTarget);
+            strTarget("Some string data");
 
-            for (int i = 0; i < objects.Count; i++)
-            {
-                print($"{objects[i]}");
-            }
+            // А можно просто указать метод
+            MyGenericDelegate<int> intTarget = IntTarget;
+            intTarget(9);
+        }
 
+        private void StringTarget(string arg)
+        {
+            print($"arg in uppercase is: {arg.ToUpper()}");
+        }
+
+        private void IntTarget(int arg)
+        {
+            print($"++arg is: {++arg}");
         }
     }
 }
