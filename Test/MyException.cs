1a946b660497d2323ea138029b3ccefb048ff149
jevlogin <jev.login@mail.ru>
2020-10-30 19:34:50 +0300

    Добавил конструктор для исключения на кнопку

--- Test/MyException.cs
+++ Test/MyException.cs
@@ -2,13 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MyException : Exception
 {
     public int Value { get; }
+    public Button Button { get; }
 
     public MyException(string message, int value) : base(message)
     {
         Value = value;
+    }
+
+    public MyException(string message, Button button) : base(message)
+    {
+        Button = button;
     }
 }
