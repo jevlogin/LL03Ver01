a094befcf9e39cd5dc48c2507738514620b7165c
jevlogin <jev.login@mail.ru>
2020-10-30 19:12:19 +0300

    С версии C# 6.0 мы можем объявлять переменные прямо при передаче в параметр

--- Test/Test.cs
+++ Test/Test.cs
@@ -9,28 +9,21 @@ namespace JevLogin
     {
         private void Start()
         {
-            Division("5", "1");
+            int x = 1;
+            int y = 0;
+            try
+            {
+                int result = x / y;
+            }
+            catch (Exception ex) when (y == 0)
+            {
+                Debug.LogError($"y не должен быть равен нулю");
+            }
+            catch(Exception ex)
+            {
+                Debug.LogError($"{ex.Message}");
+            }
         }
 
-        private void Division(string inputA, string inputB)
-        {
-            if (!Int32.TryParse(inputA, out var a))
-            {
-                Debug.Log($"Плохое число {inputA}");
-            }
-            else if (!Int32.TryParse(inputB, out var b))
-            {
-                Debug.Log($"Плохое число {inputB}");
-            }
-            else if (b == 0)
-            {
-                Debug.Log($"Деление на ноль {b}");
-            }
-            else
-            {
-                var x = a / b;
-                Debug.Log($"{a} / {b} = {x}");
-            }
-        }
     }
 }
