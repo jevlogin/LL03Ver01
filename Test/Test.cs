233f5c4c3333369456539bf87c0a0e9b78ef5535
jevlogin <jev.login@mail.ru>
2020-10-30 13:13:10 +0300

    Делегаты Action<> и Func<>

--- Test/Test.cs
+++ Test/Test.cs
@@ -8,29 +8,11 @@ namespace JevLogin
 {
     public sealed class Test : MonoBehaviour
     {
-        // Этот обобщенный делегат может вызывать любой метод, который возвращает void и принимает 
-        //  единственный параметр типа
-        private delegate void MyGenericDelegate<T>(T arg);
-        
         private void Start()
         {
-            // Зарегистрировать цели
-            MyGenericDelegate<string> strTarget = new MyGenericDelegate<string>(StringTarget);
-            strTarget("Some string data");
-
-            // А можно просто указать метод
-            MyGenericDelegate<int> intTarget = IntTarget;
-            intTarget(9);
-        }
-
-        private void StringTarget(string arg)
-        {
-            print($"arg in uppercase is: {arg.ToUpper()}");
-        }
-
-        private void IntTarget(int arg)
-        {
-            print($"++arg is: {++arg}");
+            var example = new ActionDelegateExample();
+            example[UserAction.Up]();
+            example[UserAction.Down]();
         }
     }
 }
