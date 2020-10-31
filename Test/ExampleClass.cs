14f32cdecee7818075657c3e9eef510cab3767ef
jevlogin <jev.login@mail.ru>
2020-10-31 21:04:12 +0300

    Создание новой ветки. Создал новый класс     public sealed class ListExecuteObject : IEnumerator, IEnumerable

---
+++ Test/ExampleClass.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace JevLogin
+{
+    public sealed class ExampleClass : MonoBehaviour
+    {
+        private void Start()
+        {
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            Renderer rend = go.GetComponent<Renderer>();
+            rend.material.mainTexture = Resources.Load("glass") as Texture;
+        }
+    }
+}
