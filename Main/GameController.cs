4bb212f69f8b45851363f0028236618ed080c972
jevlogin <jev.login@mail.ru>
2020-11-02 12:04:42 +0300

    refactoring class GameController

--- Main/GameController.cs
+++ Main/GameController.cs
@@ -1,17 +1,45 @@
 ﻿using System;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 namespace JevLogin
 {
-    public sealed class GameController : MonoBehaviour
+    public sealed class GameController : MonoBehaviour, IDisposable
     {
+        #region Fields
+
         private ListExecuteObject _interactiveObject;
+        private DisplayEndGame _displayEndGame;
+        private DisplayBonuses _displayBonuses;
+
+        private int _countBonuses;
+
+        #endregion
+
+
+        #region UnityMethods
 
         private void Awake()
         {
             _interactiveObject = new ListExecuteObject();
+            _displayEndGame = new DisplayEndGame();
+            _displayBonuses = new DisplayBonuses();
+            foreach (var soloObject in _interactiveObject)
+            {
+                if (soloObject is BadBonus badBonus)
+                {
+                    badBonus.OnCaughtPlayerChange += CaughtPlayer;
+                    badBonus.OnCaughtPlayerChange += _displayEndGame.GameOver;
+                }
+
+                if (soloObject is GoodBonus goodBonus)
+                {
+                    goodBonus.OnPointChange += AddBonuse;
+                }
+            }
         }
+
+
 
         private void Update()
         {
@@ -26,5 +54,40 @@ namespace JevLogin
                 interactiveObject.Execute();
             }
         }
+
+        #endregion
+
+
+        #region Methods
+
+        private void AddBonuse(int value)
+        {
+            _countBonuses += value;
+            _displayBonuses.Display(_countBonuses);
+        }
+
+        private void CaughtPlayer(string value, Color arg)
+        {
+            Time.timeScale = 0.0f;
+        }
+
+        public void Dispose()
+        {
+            foreach (var soloObject in _interactiveObject)
+            {
+                if (soloObject is BadBonus badBonus)
+                {
+                    badBonus.OnCaughtPlayerChange -= CaughtPlayer;
+                    badBonus.OnCaughtPlayerChange -= _displayEndGame.GameOver;
+                }
+
+                if (soloObject is GoodBonus goodBonus)
+                {
+                    goodBonus.OnPointChange -= AddBonuse;
+                }
+            }
+        }
+
+        #endregion
     }
 }
