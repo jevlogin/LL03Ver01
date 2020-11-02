063917b390c3c8eac1970df216af8d64d340af6d
jevlogin <jev.login@mail.ru>
2020-11-02 11:41:35 +0300

    refactoring class BadBonus

--- Bonus/BadBonus.cs
+++ Bonus/BadBonus.cs
@@ -7,17 +7,42 @@ namespace JevLogin
 {
     public sealed class BadBonus : InteractiveObject, IFlay, IRotation
     {
+        #region Fields
+
+        public event Action<string, Color> OnCaughtPlayerChange = delegate (string str, Color color) { };
+
         private float _lengthFlay;
         private float _speedRotation;
+
+        #endregion
+
+
+        #region UnityMethods
 
         private void Awake()
         {
             _lengthFlay = Range(1.0f, 5.0f);
             _speedRotation = Range(10.0f, 50.0f);
         }
-        
+
+        #endregion
+
+
+        #region Methods
+
+        public override void Execute()
+        {
+            if (!IsInteractable)
+            {
+                return;
+            }
+            Flay();
+            Rotation();
+        }
+
         protected override void Interaction()
         {
+            OnCaughtPlayerChange.Invoke(gameObject.name, _color);
         }
 
         public void Flay()
@@ -30,8 +55,6 @@ namespace JevLogin
             transform.Rotate(Vector3.up * (Time.deltaTime * _speedRotation), Space.World);
         }
 
-        public override void Execute()
-        {
-        }
+        #endregion
     }
 }
