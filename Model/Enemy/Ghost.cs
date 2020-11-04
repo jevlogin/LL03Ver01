0fb753b612add479384378a79e1f0b7bcff6aed0
jevlogin <jev.login@mail.ru>
2020-11-04 19:28:43 +0300

    Надо решить проблему с Трансформом

--- Model/Enemy/Ghost.cs
+++ Model/Enemy/Ghost.cs
@@ -8,33 +8,37 @@ namespace JevLogin
 {
     public class Ghost : InteractiveObject
     {
-        private NavMeshAgent _navMeshAgent;
-        private Transform[] _waypoints;
+        [SerializeField] private NavMeshAgent _navMeshAgent;
+        [SerializeField] private Transform[] _waypoints;
 
         private int _currentWaypointIndex;
 
-        public Ghost()
+        private void Start()
         {
-            if (_waypoints == null)
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            if (_waypoints == null || _waypoints.Length == 0)
             {
                 _waypoints = new[] { transform };
             }
-            Transform tempTransform = transform;
+            //Transform tempTransform = 
 
-            tempTransform.position = new Vector3(transform.position.x + Random.Range(2, 5), transform.position.y, transform.position.z + Random.Range(2, 5));
+            //tempTransform.position = new Vector3(transform.position.x + Random.Range(2, 5), transform.position.y, transform.position.z + Random.Range(2, 5));
 
+            print(_waypoints.Length);
             Array.Resize(ref _waypoints, _waypoints.Length + 1);
+            print(_waypoints.Length);
             _waypoints[_waypoints.Length - 1] = tempTransform;
-        }
-
-        private void Start()
-        {
+            print(_waypoints[1].position);
             _navMeshAgent.SetDestination(_waypoints[0].position);
         }
 
         public override void Execute()
         {
-            throw new System.NotImplementedException();
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                _currentWaypointIndex = (_currentWaypointIndex + 1) % _waypoints.Length;
+                _navMeshAgent.SetDestination(_waypoints[_currentWaypointIndex].position);
+            }
         }
 
         protected override void Interaction()
