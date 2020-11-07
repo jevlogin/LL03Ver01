2de3bbd8254f0d4510bd7584d015c81b2bafab4f
jevlogin <jev.login@mail.ru>
2020-11-07 22:58:02 +0300

    подготовка ветки к новому уроку

--- Test/Test.cs
+++ Test/Test.cs
@@ -9,19 +9,7 @@ namespace JevLogin
     {
         private void Start()
         {
-            try
-            {
-                int x = int.Parse("-3");
-                if (x < 0)
-                {
-                    throw new MyException("Введено недопостимое значение", x);
-                    Debug.Log("OK");
-                }
-            }
-            catch (MyException e)
-            {
-                Debug.Log($"{e.Message} {e.Value}");
-            }
+
         }
 
     }
