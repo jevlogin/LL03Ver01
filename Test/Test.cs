836e45e5ad9961f1db452ae9d17101d9930071a2
jevlogin <jev.login@mail.ru>
2020-11-07 23:07:31 +0300

    ArrayList

--- Test/Test.cs
+++ Test/Test.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.IO;
 using UnityEngine;
 
@@ -9,7 +10,15 @@ namespace JevLogin
     {
         private void Start()
         {
-
+            ArrayList list = new ArrayList();
+            list.Add(1);
+            list.Add(3.14f);
+            list.Add("Text");
+            list.Add(new int[] { 1, 2, 3 });
+            foreach (var element in list)
+            {
+                Debug.Log($"{element}");
+            }
         }
 
     }
