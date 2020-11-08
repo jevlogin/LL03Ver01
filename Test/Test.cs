ecfae8f0267bd023f2d97d59dddf0c471d2c7750
jevlogin <jev.login@mail.ru>
2020-11-08 11:18:10 +0300

    Извлечение уникальных элементов из коллекции

--- Test/Test.cs
+++ Test/Test.cs
@@ -13,6 +13,23 @@ namespace JevLogin
             IEnumerable<int> reversed = array.Reverse<int>();
         }
 
+        private ICollection<T> GetUniques<T>(ICollection<T> list)
+        {
+            // Для отслеживания элементов используйте словарь 
+            Dictionary<T, bool> found = new Dictionary<T, bool>();
+            List<T> uniques = new List<T>();
+            // Этот алгоритм сохраняет оригинальный порядок элементов
+            foreach (T val in list)
+            {
+                if (!found.ContainsKey(val))
+                {
+                    found[val] = true;
+                    uniques.Add(val);
+                }
+            }
+            return uniques;
+        }
+
         private void Reverse<T>(T[] array)
         {
             int left = 0;
