9e0c5e416d6b003610dae3f878c482bcad53b059
jevlogin <jev.login@mail.ru>
2020-11-08 11:07:11 +0300

    Изменение порядка элементов массива на обратный

--- Test/Test.cs
+++ Test/Test.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
@@ -11,21 +9,22 @@ namespace JevLogin
     {
         private void Start()
         {
-            ArrayList list = new ArrayList();
-            list.Add(1);
-            list.Add(3.14f);
-            list.Add("Text");
-            list.Add(new int[] { 1, 2, 3 });
-            foreach (var element in list)
-            {
-                Debug.Log($"{element}");
-            }
-            Debug.Log($"------------------");
-
-            List<int> vs = new List<int>() { 4, 5, 6, 9, 5, 7, 6, 1 };
-            vs.Sort();
-            Debug.Log(vs.BinarySearch(6));
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+            IEnumerable<int> reversed = array.Reverse<int>();
         }
 
+        private void Reverse<T>(T[] array)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+            while (left < right)
+            {
+                T temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+                left++;
+                right--;
+            }
+        }
     }
 }
