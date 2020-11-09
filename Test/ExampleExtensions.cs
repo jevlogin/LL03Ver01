b6abeffa560873d8a0a46d4f81786c9d1ddb1e8f
jevlogin <jev.login@mail.ru>
2020-11-09 10:19:44 +0300

    create static class ExampleExtensions

---
+++ Test/ExampleExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JevLogin
+{
+    public static class ExampleExtensions
+    {
+
+
+        public static T AddTo<T>(this T self, ICollection<T> coll)
+        {
+            coll.Add(self);
+            return self;
+        }
+    }
+}
+
