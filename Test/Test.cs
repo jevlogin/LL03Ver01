c23d4a489e537f03ca68a5d5dac412b8fe4ce084
jevlogin <jev.login@mail.ru>
2020-11-09 13:25:20 +0300

    Простой Linq-запрос

--- Test/Test.cs
+++ Test/Test.cs
@@ -10,91 +10,24 @@ namespace JevLogin
     {
         private void Start()
         {
-            var a = gameObject.GetOrAddComponent<Renderer>();
-            a.material.color = a.material.color.SetColorAlpha(0.3f);
-        }
-
-        private void ExampleDeepCopy()
-        {
-            User userOne = new User();
-            User userTwo = userOne.DeepCopy();
-        }
-
-        private class User
-        {
-        }
-
-        private void ExampleConcat()
-        {
-            int[] arrOne = new[] { 1, 2, 3 };
-            int[] arrTwo = new[] { 4, 5, 6 };
-
-            var t = arrOne.Concat(arrTwo).Concat(new[] { 7, 8, 9 });
-
-            for (int i = 0; i < t.Length; i++)
+            // Все массивы в С# неявным образом преобразуются в форму интерфейса IEnumerable<T>
+            // Благодаря этому любой массив в С# может служить в качестве источника данных, извлекаемых 
+            // по запросу Linq 
+            int[] nums = { 1, -2, 3, 0, -4, 5 };
+            // Создадим запрос, получающий только положительные числа  
+            // posNums – переменная запроса
+            // Ей присваивается результат выполнения запроса
+            var posNums = from n       // n -переменная диапазона (как в foreach)
+                    in nums  // Источник данных
+                          where n > 0  // Предикат (условие) – фильтр данных
+                          select n;      // Какие данные получаем? В сложных запросах здесь можно указать,например, фамилию адресата вместо всего адреса
+            Debug.Log("Положительные числа: ");
+            // Выполняем запрос и выводим положительные числа на экран
+            foreach (int i in posNums)
             {
-                Debug.Log(t[i]);
+                Debug.Log(i + " ");
             }
         }
 
-        private void ExampleGetOrAddComponent()
-        {
-            gameObject.GetOrAddComponent<Rigidbody>();
-        }
-
-        private void ExampleIsOneOf()
-        {
-            var arr = new[] { 1, 5, 6, 3 };
-
-            var t = 7;
-
-            foreach (var i in arr)
-            {
-                if (i == 7)
-                {
-                    Debug.Log($"Нашли с помощью foreach");
-                }
-                else
-                {
-                    Debug.Log($"Not");
-                }
-            }
-
-            if (t.IsOneOf(5, 8, 6, 3, 7))
-            {
-                Debug.Log($"Нашли с помощью метода расширения");
-            }
-            else
-            {
-                Debug.Log($"Not");
-            }
-        }
-
-        private void ExampleTryBool(string v)
-        {
-            bool res = v.TryBool();
-            Debug.Log(res);
-        }
-
-        private void AddToList()
-        {
-            var list = new List<int>();
-            var list2 = new List<int>();
-
-            list.Add(1);
-
-            1.AddTo(list).AddTo(list2);
-            2.AddTo(list2);
-
-            foreach (var l1 in list)
-            {
-                Debug.Log(l1);
-            }
-
-            foreach (var l2 in list2)
-            {
-                Debug.Log(l2);
-            }
-        }
     }
 }
