ccf045b1eed61da1ef7973c53f4eae9da6f1027f
jevlogin <jev.login@mail.ru>
2020-11-12 10:14:48 +0300

    create class StreamData : IData<SaveData>

---
+++ SaveData/StreamData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+namespace JevLogin
+{
+    public class StreamData : IData<SaveData>
+    {
+        public SaveData Load(string path = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save(SaveData data, string path = null)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+
+            }
+        }
+    }
+}
