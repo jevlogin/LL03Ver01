2e9e3623bb5807f9d9c7173291f8719670a616cf
jevlogin <jev.login@mail.ru>
2020-11-12 09:46:16 +0300

    create struct Vector3Serializable and refactoring class SaveData

---
+++ Helper/SaveData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace JevLogin
+{
+    [Serializable]
+    public sealed class SaveData
+    {
+        public string Name;
+        public Vector3Serializable Position;
+        public bool IsEnabled;
+
+        public SaveData() 
+        {
+           
+        }
+    }
+}
