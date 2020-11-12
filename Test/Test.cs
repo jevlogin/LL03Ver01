b52c5f13de4da74aec90912402c0a1f1c6505536
jevlogin <jev.login@mail.ru>
2020-11-12 09:37:29 +0300

    Готовим новую ветку

--- Test/Test.cs
+++ Test/Test.cs
@@ -8,17 +8,7 @@ namespace JevLogin
 {
     public sealed class Test : MonoBehaviour
     {
-        public class EmailAddress
-        {
-            public string Name { get; }
-            public string Address { get; }
-            public EmailAddress(string n, string a)
-            {
-                Name = n;
-                Address = a;
-            }
 
-        }
         private void Start()
         {
             ExampleLinq exampleLinq = new ExampleLinq();
