851799662fa388379c937fb8789b7e2b73e7b5ad
jevlogin <jev.login@mail.ru>
2020-11-12 12:31:56 +0300

    refactorinig class XMLData.cs create SaveData/XmlNameAttributes.cs

--- SaveData/XMLData.cs
+++ SaveData/XMLData.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml;
+﻿using System.IO;
+using System.Xml;
 using UnityEngine;
 
 
@@ -6,36 +7,72 @@ namespace JevLogin
 {
     public sealed class XMLData : IData<SaveData>
     {
-        public SaveData Load(string path = null)
+        public SaveData Load(string path = "")
         {
-            throw new System.NotImplementedException();
+            var result = new SaveData();
+            if (!File.Exists(path))
+            {
+                return result;
+            }
+            using (var reader = new XmlTextReader(path))
+            {
+                while (reader.Read())
+                {
+                    var key = XmlNameAttributes.Name;
+                    if (reader.IsStartElement(key))
+                    {
+                        result.Name = reader.GetAttribute(XmlNameAttributes.Value);
+                    }
+                    key = XmlNameAttributes.PositionX;
+                    if (reader.IsStartElement(key))
+                    {
+                        result.Position.X = reader.GetAttribute(XmlNameAttributes.Value).TrySingle();
+                    }
+                    key = XmlNameAttributes.PositionY;
+                    if (reader.IsStartElement(key))
+                    {
+                        result.Position.Y = reader.GetAttribute(XmlNameAttributes.Value).TrySingle();
+                    }
+                    key = XmlNameAttributes.PositionZ;
+                    if (reader.IsStartElement(key))
+                    {
+                        result.Position.Z = reader.GetAttribute(XmlNameAttributes.Value).TrySingle();
+                    }
+                    key = XmlNameAttributes.IsEnable;
+                    if (reader.IsStartElement(key))
+                    {
+                        result.IsEnabled = reader.GetAttribute(XmlNameAttributes.Value).TryBool();
+                    }
+                }
+            }
+            return result;
         }
 
-        public void Save(SaveData data, string path = null)
+        public void Save(SaveData data, string path = "")
         {
             var xmlDoc = new XmlDocument();
 
-            XmlNode rootNode = xmlDoc.CreateElement("Player");
+            XmlNode rootNode = xmlDoc.CreateElement(XmlNameAttributes.Player);
             xmlDoc.AppendChild(rootNode);
 
-            var element = xmlDoc.CreateElement("Name");
-            element.SetAttribute("value", data.Name);
+            var element = xmlDoc.CreateElement(XmlNameAttributes.Name);
+            element.SetAttribute(XmlNameAttributes.Value, data.Name);
             rootNode.AppendChild(element);
 
-            element = xmlDoc.CreateElement("PosX");
-            element.SetAttribute("value", data.Position.X.ToString());
+            element = xmlDoc.CreateElement(XmlNameAttributes.PositionX);
+            element.SetAttribute(XmlNameAttributes.Value, data.Position.X.ToString());
             rootNode.AppendChild(element);
 
-            element = xmlDoc.CreateElement("PosY");
-            element.SetAttribute("value", data.Position.Y.ToString());
+            element = xmlDoc.CreateElement(XmlNameAttributes.PositionY);
+            element.SetAttribute(XmlNameAttributes.Value, data.Position.Y.ToString());
             rootNode.AppendChild(element);
 
-            element = xmlDoc.CreateElement("PosZ");
-            element.SetAttribute("value", data.Position.Z.ToString());
+            element = xmlDoc.CreateElement(XmlNameAttributes.PositionZ);
+            element.SetAttribute(XmlNameAttributes.Value, data.Position.Z.ToString());
             rootNode.AppendChild(element);
 
-            element = xmlDoc.CreateElement("IsEnabled");
-            element.SetAttribute("value", data.IsEnabled.ToString());
+            element = xmlDoc.CreateElement(XmlNameAttributes.IsEnable);
+            element.SetAttribute(XmlNameAttributes.Value, data.IsEnabled.ToString());
             rootNode.AppendChild(element);
 
             XmlNode userNode = xmlDoc.CreateElement("Info");
---
+++ SaveData/XmlNameAttributes.cs
@@ -0,0 +1,15 @@
+﻿namespace JevLogin
+{
+    public static class XmlNameAttributes
+    {
+        public const string Player = "Player";
+        public const string Name = "Name";
+        public const string Position = "Position";
+        public const string PositionX = "PositionX";
+        public const string PositionY = "PositionY";
+        public const string PositionZ = "PositionZ";
+        public const string Value = "value";
+        public const string IsEnable = "IsEnable";
+
+    }
+}
