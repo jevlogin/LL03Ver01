5e10eab765419a6109738fc20a24581f993e6f42
jevlogin <jev.login@mail.ru>
2020-11-22 20:56:03 +0300

    create template     public sealed class ScriptableObjectName : ScriptableObject

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JevLogin
+{
+    public sealed class NewBehaviourScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+
+            
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            
+        }
+    }
+}
