c87028b7af315dd74bd9ac450edb1fc4df1a3ffe
jevlogin <jev.login@mail.ru>
2020-11-24 00:55:23 +0300

    свое меню  в контекстном меню

--- Helper/MenuItems.cs
+++ Helper/MenuItems.cs
@@ -16,5 +16,36 @@ namespace JevLogin
         {
 
         }
+
+        [MenuItem("Geekbrains/Пункт меню №2 %g")]
+        private static void NewNestedOption()
+        {
+        }
+
+        [MenuItem("Geekbrains/Пункт меню №3 _g")]
+        private static void NewOptionWithHotkey()
+        {
+        }
+
+        [MenuItem("Geekbrains/Пункт меню/Пункт меню №3 _g")]
+        private static void NewOptionWithHot()
+        {
+        }
+
+        [MenuItem("Assets/Geekbrains")]
+        private static void LoadAdditiveScene()
+        {
+        }
+
+        [MenuItem("Assets/Create/Geekbrains")]
+        private static void AddConfig()
+        {
+        }
+
+        [MenuItem("CONTEXT/Rigidbody/Geekbrains")]
+        private static void NewOpenForRigidBody()
+        {
+        }
+
     }
 }
