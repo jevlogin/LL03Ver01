4e7a2ced99f902bd5ababf791565a38d1bbab628
jevlogin <jev.login@mail.ru>
2020-12-02 19:38:53 +0300

    кортежи

---
+++ Test/ExampleTuple.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JevLogin
+{
+    public sealed class ExampleTuple : MonoBehaviour
+    {
+        private int _maxHP;
+        private int _currentHP;
+
+        public (int currentHP, int maxHP) GetHP()
+        {
+            return (_currentHP, _maxHP);
+        }
+
+        private void Start()
+        {
+            (int currentHP, int maxHP) playerHp = (42, 100);
+            Debug.Log($"{playerHp.currentHP}/{playerHp.maxHP}");
+        }
+    }
+}
