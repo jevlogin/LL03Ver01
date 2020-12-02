9e1d4da366502f65924a996e9ac005527318ba51
jevlogin <jev.login@mail.ru>
2020-12-02 21:22:03 +0300

    ref передача по ссылке

---
+++ Test/ExampleRefLocal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JevLogin
+{
+    public sealed class ExampleRefLocal : MonoBehaviour
+    {
+        private void Start()
+        {
+            int temp = 42;
+            ref int tempReference = ref temp;
+            Debug.Log(temp);
+            tempReference = 123;
+            Debug.Log(temp);
+            temp = 321;
+            Debug.Log(tempReference);
+            int tempSecond = 24;
+            tempReference = ref tempSecond;
+            Debug.Log(tempReference);
+
+
+        }
+    }
+}
