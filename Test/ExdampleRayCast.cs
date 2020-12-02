fa42936753f86774d2c18d0a6cfdf3959b699448
jevlogin <jev.login@mail.ru>
2020-12-02 20:02:52 +0300

    Теперь переменные out можно объявлять в списке аргументов в вызове метода, не записывая отдельный оператор объявления.

---
+++ Test/ExdampleRayCast.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JevLogin
+{
+    public sealed class ExdampleRayCast : MonoBehaviour
+    {
+        private Camera _cams;
+
+        private void Start()
+        {
+            _cams = Camera.main;
+
+            //RaycastHit raycastHit;
+            //if (Physics.Raycast(_cams.ScreenPointToRay(Input.mousePosition), out raycastHit, 100.0f))
+            if (Physics.Raycast(_cams.ScreenPointToRay(Input.mousePosition), out var hit, 100.0f))
+            {
+
+            }
+        }
+    }
+}
