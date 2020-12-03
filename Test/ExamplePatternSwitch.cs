e4ba2f9acbaa3c4f1a78c06791abe9814fe67299
jevlogin <jev.login@mail.ru>
2020-12-03 17:19:16 +0300

    Позиционный паттерн

--- Test/ExamplePatternSwitch.cs
+++ Test/ExamplePatternSwitch.cs
@@ -10,10 +10,10 @@ namespace JevLogin
     {
         private enum Gender
         {
-            None        = 0,
-            Male        = 1,
-            Female      = 2,
-            Indefined   = 3
+            None = 0,
+            Male = 1,
+            Female = 2,
+            Indefined = 3
         }
 
         private sealed class Student
@@ -23,10 +23,37 @@ namespace JevLogin
             public string AcademicPerformance { get; set; }
         }
 
+        private sealed class People
+        {
+            public string Name { get; set; }
+            public Gender Gender { get; set; }
+            public int Age { get; set; }
+
+            public void Deconstruct(out string name, out Gender gender, out int age)
+            {
+                name = Name;
+                gender = Gender;
+                age = Age;
+            }
+        }
+
 
         private void Start()
         {
-            Debug.Log(Select(Gender.Female, 12));
+            Debug.Log(Select(new People()));
+        }
+
+        private string Select(People people)
+        {
+            return people switch
+            {
+                ("Lera", Gender.Female, 18) => "Female",
+                ("Roman", Gender.Male, _) => "Male",
+                ("Ilya", Gender.Indefined, _) => "Dean",
+                (_, _, 17) => "Minor",
+                (_, Gender.Indefined, _) => "Indefined",
+                _ => "Not recognized"
+            };
         }
 
         private string Select(Gender gender, int age)
