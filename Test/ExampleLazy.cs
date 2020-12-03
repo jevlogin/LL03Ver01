84f22594f28c8d9bc0f786d1ddc5be4d65cc3b0c
jevlogin <jev.login@mail.ru>
2020-12-03 17:52:25 +0300

    Singleton превратили обычный класс в статик, класс одиночка

---
+++ Test/ExampleLazy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JevLogin
+{
+    public sealed class ExampleLazy : MonoBehaviour
+    {
+        public sealed class Singleton
+        {
+            private Singleton() { }
+
+            private static readonly Lazy<Singleton> _instance = new Lazy<Singleton>(() => new Singleton());
+
+            public static Singleton Instance => _instance.Value;
+
+            public void Test()
+            {
+                Debug.Log("Hello World");
+            }
+        }
+
+        private void Start()
+        {
+            Singleton.Instance.Test();
+        }
+    }
+}
